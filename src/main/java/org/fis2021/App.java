50f130dc6303aecc329ebfd7f6424943c04feb51
Maria <ruviniantumaria@yahoo.com>
2021-04-18 17:02:46 +0300

    Rename project

--- src/main/java/org/fis2021/App.java
+++ src/main/java/org/fis2021/App.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.fis2021;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- src/main/java/org/fis2021/SecondaryController.java
+++ src/main/java/org/fis2021/SecondaryController.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.fis2021;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
